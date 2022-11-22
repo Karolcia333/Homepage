3c134b9c4f19fd0d3bf127bb48cbce26a7ef4dfa
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2022-11-22 10:59:48 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+{
+    const welcome = () => {
+    console.log("Witam wszystkich developerów🙂");
+ };
+
+const toggleBackground = () => {
+    const body = document.querySelector(".body");
+    const navigation__button = document.querySelector(".js-themeName");
+
+
+body.classList.toggle("body--lightBlue")
+navigation__button.innerText = body.classList.contains("body--lightBlue")
+    ? "białe"
+    : "błękitne";
+ };
+
+const init = () => {
+    const button = document.querySelector(".js-navigation__button");
+    button.addEventListener("click", toggleBackground);
+
+    welcome();
+ };
+
+init();   
+}
